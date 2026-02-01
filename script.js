d596592a89b5fd169cc1765c9e20d3d94d47fce2
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-02-01 14:15:33 +0100

    added minimal interactivity

---
+++ script.js
@@ -0,0 +1,18 @@
+if (window.ScrollReveal) {
+  ScrollReveal().reveal(".intro", {
+    origin: "bottom",
+    distance: "16px",
+    duration: 600,
+    easing: "ease-in-out",
+    opacity: 0,
+  });
+
+  ScrollReveal().reveal(".card", {
+    origin: "bottom",
+    distance: "32px",
+    duration: 800,
+    easing: "ease-in-out",
+    opacity: 0,
+    interval: 150,
+  });
+}
